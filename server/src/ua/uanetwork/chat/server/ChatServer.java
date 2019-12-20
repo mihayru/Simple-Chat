b306b721b776fa5f9f80ded9c530fad726a8f03e
mihayru <mjgang258@gmail.com>
2019-12-20 19:02:28 +0200

    server

--- server/src/ua/uanetwork/chat/server/ChatServer.java
+++ server/src/ua/uanetwork/chat/server/ChatServer.java
@@ -1,10 +1,21 @@
 package ua.uanetwork.chat.server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
 public class ChatServer {
 
     public static void main(String[] args) {
+        new ChatServer();
 
 
-
+    }
+    private ChatServer(){
+        System.out.println("Server running...");
+        try(ServerSocket serverSocket = new ServerSocket(8189)) {
+            while (true)
+        }catch (IOException e ) {
+            throw new RuntimeException(e);
+        }
     }
 }

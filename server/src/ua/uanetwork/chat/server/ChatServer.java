5220cfae7d13232a02ff6907a5f98445c7ee09e9
mihayru <mjgang258@gmail.com>
2019-12-21 13:22:25 +0200

    server rdy2.0

--- server/src/ua/uanetwork/chat/server/ChatServer.java
+++ server/src/ua/uanetwork/chat/server/ChatServer.java
@@ -1,21 +1,66 @@
 package ua.uanetwork.chat.server;
 
+import ua.uanetwork.chat.network.TCPConnection;
+import ua.uanetwork.chat.network.TCPConnectionListener;
+
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.util.ArrayList;
 
-public class ChatServer {
+public class ChatServer implements TCPConnectionListener {
 
     public static void main(String[] args) {
         new ChatServer();
 
-
     }
+    private final ArrayList<TCPConnection> connections = new ArrayList<>();
+
     private ChatServer(){
         System.out.println("Server running...");
         try(ServerSocket serverSocket = new ServerSocket(8189)) {
-            while (true)
+            while (true){
+                try {
+                    new TCPConnection(this, serverSocket.accept());
+                } catch (IOException e){
+                    System.out.println("TCPConnection exception: " + e);
+                }
+            }
         }catch (IOException e ) {
             throw new RuntimeException(e);
         }
     }
-}
+
+    @Override
+    public synchronized void onConnectionReady(TCPConnection tcpConnection) {
+        connections.add(tcpConnection);
+        sendToAllConncetions("Client connected: " + tcpConnection);
+
+    }
+
+    @Override
+    public synchronized void onReceiveString(TCPConnection tcpConnection, String value) {
+        sendToAllConncetions(value);
+
+
+    }
+
+    @Override
+    public synchronized void onDisconnect(TCPConnection tcpConnection) {
+        connections.remove(tcpConnection);
+        sendToAllConncetions("Client disconnected: " + tcpConnection);
+
+    }
+
+    @Override
+    public void onException(TCPConnection tcpConnection, Exception e) {
+        System.out.println("TCPConnection exception: " + e);
+
+    }
+    private void sendToAllConncetions(String value){
+        System.out.println(value);
+        final int cnt =connections.size();
+        for (int i = 0; i < connections.size(); i++) connections.get(i).sendString(value );
+            
+        }
+
+    }

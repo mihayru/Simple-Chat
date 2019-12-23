bb9a9bd51a8455c2c21dfd6939f9c4b8ad4ca95b
mihayru <mjgang258@gmail.com>
2019-12-23 12:22:01 +0200

    Client Window rdy

--- client/src/ua/uanetwork/chat/client/ClientWindow.java
+++ client/src/ua/uanetwork/chat/client/ClientWindow.java
@@ -1,4 +1,98 @@
 package ua.uanetwork.chat.client;
 
-public class ClientWindow {
+import ua.uanetwork.chat.network.TCPConnection;
+import ua.uanetwork.chat.network.TCPConnectionListener;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+
+public class ClientWindow extends JFrame implements ActionListener, TCPConnectionListener {
+
+    private static final String IP_ADDR = "195.43.70.238";
+    private static final int PORT = 8189;
+    private static final int WIDTH = 600;
+    private static final int HEIGHT = 400;
+
+    public static void main(String[] args ) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new ClientWindow();
+            }
+        });
+    }
+
+    private final JTextArea log = new JTextArea();
+    private final JTextField fieldNickname = new JTextField("mishi");
+    private final JTextField fieldInput  = new JTextField();
+
+    private TCPConnection connection;
+
+    private ClientWindow(){
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(WIDTH,HEIGHT);
+        setLocationRelativeTo(null);
+        setAlwaysOnTop(true);
+
+        log.setEditable(false);
+        log.setLineWrap(true);
+        add(log, BorderLayout.CENTER);
+
+        fieldInput.addActionListener(this);
+        add(fieldInput, BorderLayout.SOUTH);
+        add(fieldNickname, BorderLayout.NORTH);
+
+        setVisible(true);
+        try {
+            connection = new TCPConnection(this,IP_ADDR, PORT);
+        } catch (IOException e) {
+            printMsg("Connection exception: " + e);
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String msg = fieldInput.getText();
+        if(msg.equals("")) return;
+        fieldInput.setText(null);
+        connection.sendString(fieldNickname.getText() + ": " + msg);
+
+    }
+
+    @Override
+    public void onConnectionReady(TCPConnection tcpConnection) {
+        printMsg("Connection ready...");
+
+    }
+
+    @Override
+    public void onReceiveString(TCPConnection tcpConnection, String value) {
+        printMsg(value);
+
+    }
+
+    @Override
+    public void onDisconnect(TCPConnection tcpConnection) {
+        printMsg("Connection close");
+
+    }
+
+    @Override
+    public void onException(TCPConnection tcpConnection, Exception e) {
+        printMsg("Connection exeption: " + e);
+
+    }
+    private synchronized void printMsg(String msg){
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                log.append(msg + "\n");
+                log.setCaretPosition(log.getDocument().getLength());
+            }
+        });
+    }
 }
